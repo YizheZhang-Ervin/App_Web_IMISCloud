cb6125ade83a1af3a784e1ca6ba22ca6027515d9
Yizhe Zhang <zhang.yizhe@husky.neu.edu>
2020-05-02 09:56:20 -0400

    update heroku settings

--- IMISCloud/settings.py
+++ IMISCloud/settings.py
@@ -71,15 +71,15 @@ WSGI_APPLICATION = 'IMISCloud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # heroku database
-# DATABASES = {'default': dj_database_url.config()}
+DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
