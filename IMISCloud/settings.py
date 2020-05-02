2b5054755c85266783fd7d35e153d0928764ddbd
Yizhe Zhang <zhang.yizhe@husky.neu.edu>
2020-05-02 09:55:04 -0400

    change files

--- IMISCloud/settings.py
+++ IMISCloud/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -77,9 +76,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# heroku database
-# DATABASES = {'default': dj_database_url.config()}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Cloud
 
 # Cloud Storage
 Store/View Image/Music/Video/Files
+
+# Deploy
+heroku run python manage.py makemigrations --app=imiscloud
+heroku run python manage.py migrate --app=imiscloud
+heroku run python manage.py createsuperuser --app=imiscloud
